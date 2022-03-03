6afdedfa050e3280d4010331385de17d3df43a21
Riyan Wibowo <tunggalrahayu1@gmail.com>
2022-03-04 03:00:44 +0700

    add headers

--- parsers/listing.rb
+++ parsers/listing.rb
@@ -11,6 +11,14 @@ videos.each do |video|
         page_type: "videos",
         url:fullurl,
         fetch_type: "browser",
+        headers:{
+            "user-agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
+            "sec-ch-ua" => '" Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
+            "sec-ch-ua-arch" => '"x86"',
+            "sec-ch-ua-bitness" => '"64"',
+            "sec-ch-ua-full-version" => '"98.0.4758.102"',
+            "sec-ch-ua-full-version-list" => '" Not A;Brand";v="99.0.0.0", "Chromium";v="98.0.4758.102", "Google Chrome";v="98.0.4758.102"'
+        },
         vars: {
             img_url: img_url
         }
--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -2,6 +2,14 @@ pages << {
     url: "https://www.youtube.com/playlist?list=PLWdN67pbVPSm2tixoc-mqBSGaaEsIB1Rg",
     page_type: "listing",
     fetch_type: "browser",
+    headers:{
+        "user-agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
+        "sec-ch-ua" => '" Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
+        "sec-ch-ua-arch" => '"x86"',
+        "sec-ch-ua-bitness" => '"64"',
+        "sec-ch-ua-full-version" => '"98.0.4758.102"',
+        "sec-ch-ua-full-version-list" => '" Not A;Brand";v="99.0.0.0", "Chromium";v="98.0.4758.102", "Google Chrome";v="98.0.4758.102"'
+    },
     driver: {
         code: "
             await page.waitForSelector('#stats .yt-formatted-string');
