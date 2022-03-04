7c7b233f5a7ebaef1129982ec2407e849cd21470
riyanWi <tunggalrahayu1@gmail.com>
2022-03-04 22:12:30 +0700

    fix header on videos

--- parsers/listing.rb
+++ parsers/listing.rb
@@ -12,6 +12,10 @@ videos.each do |video|
         url:fullurl,
         fetch_type: "browser",
         headers:{
+            "accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "accept-encoding" => "gzip, deflate, br",
+            "accept-language" => "en-US,en;q=0.9,id;q=0.8",
+            "cache-control" => "max-age=0",
             "user-agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
             "sec-ch-ua" => '" Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
             "sec-ch-ua-arch" => '"x86"',
